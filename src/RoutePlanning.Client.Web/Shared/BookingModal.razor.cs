9f09410d2985d480b9d5b4ebecc712abc09c5fb8
Frederik Strøm Friborg <frsf@netcompany.com>
2024-06-13 21:13:54 +0200

    created bookingmodal and made changes

---
+++ src/RoutePlanning.Client.Web/Shared/BookingModal.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using RoutePlanning.Application.Locations.Queries.SelectableLocationList;
+
+namespace RoutePlanning.Client.Web.Shared;
+
+public sealed partial class BookingModal
+{
+    [Parameter] 
+    public bool IsActive { get; set; } = false;
+
+    [Parameter]
+    public EventCallback<bool> IsActiveChanged { get; set; }
+
+    public async Task OnSelectedChanged(ChangeEventArgs e)
+    {
+        await IsActiveChanged.InvokeAsync(!IsActive);
+    }
+}
---
+++ src/RoutePlanning.Client.Web/Shared/ListView.razor.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using RoutePlanning.Application.Locations.Queries.SelectableLocationList;
+
+namespace RoutePlanning.Client.Web.Shared;
+
+public sealed partial class ListView
+{
+    private bool ShowModal { get; set; } = false;
+
+    [Inject]
+    private IMediator Mediator { get; set; } = default!;
+    private void OnClickShowModal()
+    {
+        ShowModal = !ShowModal;
+    }
+}
